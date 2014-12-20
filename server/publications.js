f7afbe8a8261eaffe75ca0298eaa5e7461fb8d1b
Marcel Bender <marcel.bender@gmail.com>
2014-12-20 20:19:59 +0100

    Update Meteor to 1.0.2

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,19 @@
 if (Meteor.isServer) {
     Meteor.startup(function () {
+        /*
+        // Publish non deleted posts filtered by userId
         Meteor.publish("posts", function() {
-            return Posts.find({deletedAt: { $exists: false }}, {sort: {created: -1}});
+            if (this.userId) {
+                check(this.userId, String);
+                return Posts.find({ userId: this.userId }, { deletedAt: { $exists: false }}, {sort: {created: -1}});
+            }
+        })
+        */
+
+        // Publish non deleted posts without filtering by userId
+        Meteor.publish("posts", function() {
+          return Posts.find({deletedAt: { $exists: false }}, {sort: {created: -1}});
         });
     });
 }
+
